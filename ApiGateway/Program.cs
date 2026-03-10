2967294ce7aa03be748cb900ab335fb1bfe11ec5
SureshraviK <krssureshrrr@gmail.com>
2026-03-10 07:46:13 +0530

    Log_write completed

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -24,10 +24,26 @@
 
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("ocelot.json");
+
+// Configure Serilog
+//Serilog.AspNetCore: Integrates Serilog with ASP.NET Core, replacing the default logging provider.
+//Serilog.Sinks.Console: Enables log output to the console, which is very useful during development for real-time log monitoring.
+//Serilog.Sinks.File: Enables logging to files on disk. This is useful for persistent storage, auditing, and post-event analysis.
+//Serilog.Settings.Configuration: Allows Serilog to be configured via appsettings.json, so you can change log settings without modifying code.
+//Serilog.Sinks.Async: Wraps logging sinks with asynchronous functionality to improve performance by offloading log processing to a background thread.
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.Console()
+    .WriteTo.File("logs/apigateway.log", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
 
 builder.Services.AddOcelot();
 
