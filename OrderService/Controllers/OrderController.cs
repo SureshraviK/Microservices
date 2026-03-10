80bd0f614d9c4dd8c2066cdebcebcc8bef1fa163
Suresh R <Suresh R@LAPTOP-3J9PGAU2>
2026-03-10 06:34:36 +0530

    Add project files.

---
+++ OrderService/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderService.Models;
+
+namespace OrderService.Controllers
+{
+    [Route("api/orders")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private static List<Order> orders = new List<Order>();
+
+        [HttpGet]
+        public IActionResult GetOrders()
+        {
+            return Ok(orders);
+        }
+
+        [HttpPost]
+        public IActionResult CreateOrder(Order order)
+        {
+            orders.Add(order);
+            return Ok(order);
+        }
+    }
+}
---
+++ ProductService/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductService.Models;
+
+namespace ProductService.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>()
+    {
+        new Product { Id = 1, Name = "Laptop", Price = 80000 },
+        new Product { Id = 2, Name = "Mobile", Price = 20000 }
+    };
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            products.Add(product);
+            return Ok(product);
+        }
+    }
+}
